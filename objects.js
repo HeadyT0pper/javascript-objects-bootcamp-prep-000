7f368dbf3db475d250d0e3fdd1c5361195f01773
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-02 18:10:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,6 @@ function updatePlaylist(obj, key, value) {
   obj[key] = value
   return obj
 }
-function removeFromPlaylist(obj, key, value) {
-  obj[key] = value
+function removeFromPlaylist(MichaelBuble: 'Home') {
   delete 
 }
